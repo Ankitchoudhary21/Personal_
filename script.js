59cea9aea4307f2defc682a29b6a0ffba97cce31
ankitchoudhary21 <ankitvolley7@gmail.com>
2024-02-27 11:41:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+document.getElementById("githubLink").addEventListener("click", () => {
+  window.open("https://github.com/Ankitchoudhary21", "_blank");
+});
+
+
+document.getElementById("linkedinLink").addEventListener("click", () => {
+  window.open("https://www.linkedin.com/in/ankit-choudhary-/", "_blank");
+});
+
+document.getElementById("contactLink").addEventListener("click", () => {
+  document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("resumeLink").addEventListener("click", () => {
+  window.open(
+    "https://drive.google.com/file/d/1CzUjjMCYWwLOb_yyz5_BarCN1OMaPqI6/view?usp=sharing"
+  );
+});
+ 
